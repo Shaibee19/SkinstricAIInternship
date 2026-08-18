0390fce74bed5a18637620951a66d33ee58db95c
Shaibee19 <shaipattzgray@gmail.com>
2026-08-18 12:07:05 -0400

    home page is looking very close to 1-1, minor tweaks possible

--- src/home/page.jsx
+++ src/home/page.jsx
@@ -1,3 +1,8 @@
+import leftRect from "../assets/home/home-rectangle-left.png";
+import rightRect from "../assets/home/home-rectangle-right.png";
+import iconLeft from "../assets/home/button-icon-shrunk-left.png";
+import iconRight from "../assets/home/button-icon-shrunk-right.png";
+
 export default function HomePage() {
     return (
         <div className="max-sm:scale-[0.75] max-sm:origin-center max-sm:p-6">
@@ -37,7 +42,7 @@ export default function HomePage() {
                     ENTER EXPERIENCE
                 </span>
                 <div className="w-6 h-6 border border-solid border-black rotate-45 cursor-pointer"></div>
-                <span className="absolute left-8 scale-[0.5] hover:scale-60 duration-300">
+                <span className="absolute left-32 scale-[0.5] hover:scale-60 duration-300">
                     <svg
                     viewBox="0 0 24 24"
                     width="24"
@@ -53,7 +58,6 @@ export default function HomePage() {
 
             {/* Paragraph - Desktop */}
             <div className="hidden lg:block fixed bottom-[calc(-7vh)] left-[calc(-20vw)] xl:left-[calc(-27vw)] 2xl:left-[calc(-31vw)] font-normal text-sm text-[#1A1B1C] space-y-3 uppercase">
-            {/* [@media(width&gt;=1920px)]:left-[calc(-33vw)]  */}
             <p>
                 Skinstric developed an A.I. that creates a
                 <br />
@@ -66,21 +70,23 @@ export default function HomePage() {
             {/* Left Button */}
             <div
             id="left-section"
-            className="hidden lg:block fixed left-[calc(-53vw)] top-1/2 -translate-y-1/2 w-[500px] h-[500px]"
+            className="hidden lg:block fixed left-[calc(-53vw)] top-1/2 -translate-y-1/2 w-[500px] h-[500px] xl:left-[calc(-50vw)] transition-opacity duration-500 ease-in-out opacity-100"
             >
-            {/* xl:left-[calc(-50vw)] ...  transition-opacity duration-500 ease-in-out opacity-100 */}
                 <div className="relative w-full h-full">
                     <div className="w-full h-full border border-dotted border-[#A0A4AB] rotate-45 absolute inset-0"></div>
 
                     <button
                     id="discover-button"
-                    className="group inline-flex items-center justify-center gap-4 text-sm font-normal text-[#1A1B1C] h-9 absolute top-1/2 right-0 -translate-y-1/2 px-3 py-1"
+                    className="group inline-flex items-center justify-center gap-4 text-sm font-normal text-[#1A1B1C] h-9 absolute top-1/2 right-0 -translate-y-1/2 px-3 py-1 whitespace-nowrap rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring cursor-pointer disabled:opacity-50 translate-x-1/5 xl:translate-x-1/6"
                     >
-                    {/* whitespace-nowrap rounded-md ... transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring cursor-pointer disabled:opacity-50 ... translate-x-1/5 xl:translate-x-1/6 [@media(width&gt;=1920px)]:translate-x-1/20 */}
                     <div className="w-[30px] h-[30px] border border-solid border-black rotate-45 cursor-pointer group-hover:scale-110 duration-300"></div>
-                    <span className="absolute left-4 top-2 scale-[0.9] rotate-180 group-hover:scale-105 duration-300">
-                        ▶
-                    </span>
+                     <svg
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    className="fill-current text-black absolute left-4 top-[7px] scale-[0.9] rotate-180 group-hover:scale-105 duration-300">
+                    <path d="M8 5v14l11-7z"></path>
+                    </svg>
                     <span>DISCOVER A.I.</span>
                     </button>
                 </div>
@@ -89,23 +95,25 @@ export default function HomePage() {
             {/* Right Button */}
             <div
             id="right-section"
-            className="hidden lg:block fixed top-1/2 right-[calc(-53vw)] -translate-y-1/2 w-[500px] h-[500px]"
+            className="hidden lg:block fixed top-1/2 right-[calc(-53vw)] -translate-y-1/2 w-[500px] h-[500px] xl:right-[calc(-50vw)] transition-opacity duration-500 ease-in-out opacity-100"
             >
-                {/*  xl:right-[calc(-50vw)] ... transition-opacity duration-500 ease-in-out opacity-100 */}
             <div className="relative w-full h-full">
                 <div className="w-full h-full border border-dotted border-[#A0A4AB] rotate-45 absolute inset-0"></div>
 
                 <a href="/testing">
                 <button
                     id="take-test-button"
-                    className="group inline-flex items-center justify-center gap-4 text-sm font-normal text-[#1A1B1C] h-9 absolute top-1/2 left-0 -translate-y-1/2 px-3 py-1"
+                    className="group inline-flex items-center justify-center gap-4 text-sm font-normal text-[#1A1B1C] h-9 absolute top-1/2 left-0 -translate-y-1/2 px-3 py-1 whitespace-nowrap rounded-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring cursor-pointer disabled:opacity-50 -translate-x-1/5 xl:-translate-x-1/6"
                 >
-                    {/* whitespace-nowrap rounded-md ... transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring cursor-pointer disabled:opacity-50 ... -translate-x-1/5 xl:-translate-x-1/6 [@media(width&gt;=1920px)]:-translate-x-1/20 */}
                     TAKE TEST
                     <div className="w-[30px] h-[30px] border border-solid border-black rotate-45 group-hover:scale-110 duration-300"></div>
-                    <span className="absolute left-[107px] top-[9px] scale-[0.9] cursor-pointer group-hover:scale-105 duration-300">
-                    ▶
-                    </span>
+                    <svg
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    className="fill-current text-black absolute left-[95px] top-[7px] scale-[0.9] cursor-pointer group-hover:scale-105 duration-300">
+                    <path d="M8 5v14l11-7z"></path>
+                    </svg>
                 </button>
                 </a>
             </div>
